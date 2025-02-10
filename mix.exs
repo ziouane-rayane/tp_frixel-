a3e55bcd5b884b6faf1d00b23a5f5f8881ceb38b
Tristan Flores <tristan.flores@inter-gestion.com>
2025-02-10 10:08:07 +0100

    IG-2 feat(docker) Ajout d'un docker-compose variabilisé pour lancer la base de données -- Ajout de credo à nos dépendances

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule IgIntranet.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
